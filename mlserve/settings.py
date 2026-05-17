935d4225d123c1ad7fd0299ee2000cdc458aa3be
htaey <118704579+htaey@users.noreply.github.com>
2026-05-17 22:04:42 +0900

    Update settings.py

--- mlserve/settings.py
+++ mlserve/settings.py
@@ -56,3 +56,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
